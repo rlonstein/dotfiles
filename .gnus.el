296dc234b7d20e15158dfe71d34dc4f55153fe9a
Ross Lonstein <rlonstein+github@pobox.com>
2014-12-04 09:15:26 -0500

    whoops, also fix remaining symlinks->hardlinks

--- .gnus.el
+++ .gnus.el
@@ -1,1 +1,14 @@
-/Users/lonstein/.gnus.el
+(setq nntp-maximum-request 3)
+(setq nntp-connection-timeout 3)
+(setq nntp-maximum-request 100)
+(setq nntp-warn-about-losing-connection t)
+(setq gnus-summary-same-subject "")
+(setq gnus-sum-thread-tree-root "")
+(setq gnus-sum-thread-tree-single-indent "")
+(setq gnus-sum-thread-tree-leaf-with-other "+-> ")
+(setq gnus-sum-thread-tree-vertical "|")
+(setq gnus-sum-thread-tree-single-leaf "`-> ")
+(setq gnus-select-method '(nnnil ""))
+
+(setq gnus-posting-styles
+      '( (message-news-p) (address "rlonstein+usenet@pobox.com")))
