be12bec4dffa52eeb0f775396cce2aba4ce426d8
Ross Lonstein <rlonstein+github@pobox.com>
2017-06-04 19:18:00 -0400

    start reorg of my zsh cfg

---
+++ zsh/zsh/10_aliases.zsh
@@ -0,0 +1,19 @@
+
+
+alias lsa='ls -A'
+alias lsd='ls -d *(/)'
+alias ll='ls -lAh'
+
+if [[ "${OS}" = "Linux" ]]; then
+  alias atime="stat -c '%X (%x) %n'"
+  alias mtime="stat -c '%Y (%y) %n'"
+  alias owner="stat -c '%U (%u) %n'"
+  alias group="stat -c '%G (%g) %n'"
+else
+  # NetBSD stat, works for OSX and some BSDs
+  alias atime="stat -f '%Sa (%a) %N'"
+  alias mtime="stat -f '%Sm (%m) %N'"
+  alias owner="stat -f '%Su (%u) %N'"
+  alias group="stat -f '%Sg (%g) %N'"
+fi
+
---
+++ zsh/zsh/30_functions.zsh
@@ -0,0 +1,8 @@
+showoptions() {                                                                                                                                                                                                     
+  local k                                                                                                                                                                                                           
+  zmodload -i zsh/parameter                                                                                                                                                                                         
+                                                                                                                                                                                                                    
+  for k in ${(ok)options}; do                                                                                                                                                                                       
+    printf "%-20s\t%s\n" $k ${options[$k]}                                                                                                                                                                          
+  done                                                                                                                                                                                                              
+}
