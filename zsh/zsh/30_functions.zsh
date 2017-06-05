78fe34b38afc2d17aa5b0be5d2d8674b80b6361b
Ross Lonstein <rlonstein+github@pobox.com>
2017-06-05 16:31:43 -0400

    and more whitespace

--- zsh/zsh/30_functions.zsh
+++ zsh/zsh/30_functions.zsh
@@ -1,2 +1,7 @@
-showoptions() {                                                                                                                                                                            local k                                                                                                                                                                                  zmodload -i zsh/parameter                                                                                                                                                                for k in ${(ok)options}; do                                                                                                                                                                printf "%-20s\t%s\n" $k ${options[$k]}                                                                                                                                                 done
+showoptions() {
+    local k
+    zmodload -i zsh/parameter
+    for k in ${(ok)options}; do
+        printf "%-20s\t%s\n" $k ${options[$k]}
+    done
 }
